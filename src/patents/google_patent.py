711c4cb769373bde16b618087066504517be5682
CuberHuber <lupashcko.rom@yandex.ru>
2025-03-28 14:28:26 +0300

    Multi-constructor implementation was changed to `multipledispatch`

--- src/patents/google_patent.py
+++ src/patents/google_patent.py
@@ -1,3 +1,5 @@
+from multipledispatch import dispatch
+
 from .abc_patent import ABCPatent
 from .extends.abc_patent_extends import ABCPatentExtends
 from .extends.empty_patent_extends import EmptyGooglePatentExtends
@@ -31,36 +33,38 @@ class GooglePatent(ABCPatent):
 
     """
 
-    def __init__(self, uri: str, row: dict | None = None, extends: ABCPatentExtends | None = None):
-        if isinstance(uri, str) and isinstance(row, dict) and isinstance(extends, ABCPatentExtends):
-            # primary constructor
-            assert row.get("uri") is not None and row.get("uri") == uri
-            super().__init__(row)
-            self._extends = extends
-            self._uri = uri
+    @dispatch(str, dict, ABCPatentExtends)
+    def __init__(self, uri: str, row: dict, extends: ABCPatentExtends):
+        # primary constructor
+        assert row.get("uri") is not None and row.get("uri") == uri
+        super().__init__(row)
+        self._extends = extends
+        self._uri = uri
 
-        elif isinstance(uri, str) and isinstance(row, dict) and extends is None:
-            # secondary constructor call prima
-            self.__init__(
-                uri=uri,
-                row=row,
-                extends=EmptyGooglePatentExtends(),
-            )
+    @dispatch(str, dict)
+    def __init__(self, uri: str, row: dict):
+        # secondary constructor call prima
+        if row.get("uri") != uri:
+            row['uri'] = uri
 
-        elif isinstance(uri, str) and row is None and extends is None:
-            # secondary constructor
-            self.__init__(
-                uri=uri,
-                row={'uri': uri},
-                extends=EmptyGooglePatentExtends(),
-            )
+        self.__init__(
+            uri=uri,
+            row=row,
+            extends=EmptyGooglePatentExtends(),
+        )
 
-        else:
-            raise AttributeError('uri must be str')
+    @dispatch(str)
+    def __init__(self, uri: str):
+        # secondary constructor
+        self.__init__(
+            uri=uri,
+            row={'uri': uri},
+            extends=EmptyGooglePatentExtends(),
+        )
 
     def with_extends(self) -> ABCPatent:
         return GooglePatent(
-            uri=self._uri,
-            row={**self.data, **self._extends.content()},
-            extends=self._extends
+            self._uri,
+            {**self.data, **self._extends.content()},
+            self._extends
         )
