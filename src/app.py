daaf9afd3dda63e5e86048aceb48d3141f242c03
CuberHuber <lupashcko.rom@yandex.ru>
2025-03-31 21:29:41 +0300

    First attempt to realize true Environment Class by EO

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from pathlib import Path
-
+from .config import Environment, KeyListPairsEnvironment
 from patent_collections.modified_collections import Limited, WithExtendsColumn
-from patent_collections.sources import PatentsFile, PatentsFromFile
+from patent_collections.sources import PatentsInFile
+from patent_collections.destinations import PatentsToFile
 
 
 class App:
@@ -26,39 +26,36 @@ class App:
         columns
     )
 
-    """
-
-    def __init__(self):
-        self.all_columns = [
+    self._all_columns = [
             'id', 'title', 'assignee', 'inventor/author', 'priority date',
             'filing/creation date', 'publication date', 'grant date',
             'uri', 'representative figure link'
         ]
-        self.columns = [
-            'id', 'title', 'assignee', 'inventor/author', 'priority date',
-            'filing/creation date', 'publication date', 'grant date', 'uri'
-        ]
-        self.data_dir = Path(__file__).parent.parent / 'data'
-        self.final_csv_path = self.data_dir / 'final.csv'
-        self.test_csv_path = self.data_dir / 'test.csv'
-        self.source_csv_path = self.data_dir / 'source.csv'
-        self.raw_csv_path = self.data_dir / 'patents_raw.csv'
-        self.max_patents = 5
+
+    """
+
+    def __init__(self):
+        self._environment = Environment(True)
+        self._key_list_pairs_environment = KeyListPairsEnvironment('::', True)
 
     def run(self):
-        patents = Limited(
-            PatentsFromFile(str(self.source_csv_path), self.columns, 0), self.max_patents
+        # Processed patent collection
+        patents = WithExtendsColumn(
+            Limited(
+                PatentsInFile(
+                    self._environment.field('SOURCE_CSV_PATH'),
+                    self._key_list_pairs_environment.field('COLUMNS'),
+                ),
+                int(self._environment.field('MAX_PATENTS')),
+            )
         )
 
-        file = PatentsFile(
-            str(self.test_csv_path),
-            WithExtendsColumn(patents),
+        # Patent Destination
+        file = PatentsToFile(
+            self._environment.field('FINAL_CSV_PATH'),
+            patents,
             True
         )
 
-        file.save()  # Сохранение файла
-
-
-if __name__ == '__main__':
-    app = App()
-    app.run()
+        # Save patents
+        file.save()
--- src/config.py
+++ src/config.py
@@ -1,11 +1,54 @@
 import os
+from abc import ABC
+
 from dotenv import load_dotenv
-load_dotenv()
+from multipledispatch import dispatch
+from typing_extensions import override
 
-SOURCE_CSV_PATH: str = os.getenv('SOURCE_CSV_PATH')
+from aspects.cachable import Cachable
 
-DATASET_PATH: str = os.getenv('DATASET_PATH')
 
-MAX_ROWS_VALUE: int = int(os.getenv('MAX_ROWS_VALUE'))
+class AbstractEnvironment(ABC):
+    _force_load: bool
 
-OUT_CSV_PATH: str = os.getenv('OUT_CSV_PATH')
+    @Cachable()
+    def _load(self):
+        if self._force_load:
+            load_dotenv()
+
+    def field(self, key: str) -> str:
+        self._load()
+        return os.getenv(key)
+
+
+class Environment(AbstractEnvironment):
+
+    @dispatch(bool)
+    def __init__(self, force_load: bool):
+        self._force_load = force_load
+
+    @dispatch()
+    def __init__(self):
+        self.__init__(False)
+
+
+class KeyListPairsEnvironment(AbstractEnvironment):
+    """
+    The key-value pairs environment when value is a string list.
+    """
+
+    @dispatch(str, bool)
+    def __init__(self, separator: str, force_load: bool):
+        """Primary constructor"""
+        self._force_load = force_load
+        self._separator = separator
+
+    @dispatch(str)
+    def __init__(self, separator: str):
+        """Secondary constructor"""
+        self.__init__(separator, False)
+
+    @override
+    def field(self, key: str) -> list[str]:
+        value = super().field(key)
+        return value.split(self._separator)
